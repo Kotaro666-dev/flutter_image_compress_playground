58c9446962f57f0472cd995443798aa59b3f3865
Kotaro Kamashima <kotaro0726@gmail.com>
2024-02-29 08:31:37 +0900

    Update: Add basic dart files

---
+++ lib/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_image_compress playground'),
+      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatefulWidget {
+  const _Body();
+
+  @override
+  State<_Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<_Body> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+            Text('WIP'),
+        ],
+      ),
+    );
+  }
+}
+
